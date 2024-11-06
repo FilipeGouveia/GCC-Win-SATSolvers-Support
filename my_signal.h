3e808e91803905aafaaf8874015e257a9dc5a031
fgouveia <filipe.gouveia@siscog.pt>
2024-11-06 20:22:04 +0000

    added signal file

---
+++ my_signal.h
@@ -0,0 +1,10 @@
+#ifndef _MY_SIGNAL_H_
+#define _MY_SIGNAL_H_
+
+#if defined(_MSC_VER) || defined(__MINGW32__)
+
+#define SIGXCPU     1338    /*24 on POSIX*/ /* CPU time limit exceeded */
+
+#endif
+
+#endif // _MY_SIGNAL_H_
